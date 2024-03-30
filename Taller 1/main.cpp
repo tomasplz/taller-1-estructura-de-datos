b8fcb336365dcf0cf22d97f878ea91d147c34b7d
tomasplz <tomasplz@hotmail.com>
2024-03-29 21:06:23 -0300

    Taller 1 Estructura de datos

---
+++ Taller 1/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "Evento.h"
+
+bool loadData();
+bool createNewEvent();
+
+using namespace std;
+
+int main(){
+    int answer;
+    cout << "Cargando los datos . . .\n\n\n"<<endl;
+    if (loadData())
+    {   
+        cout << "Bienvenido al gestor de eventos\n"<<endl;
+        while (true)
+        {
+        cout << "1) Crear nuevo evento \n2) Registro asistentes\n3) Consulta asistentes\n4) Generacion de informes"<<endl;
+        cin >> answer;
+        switch (answer) // TO DO exception when var isnt a int
+        {
+            case 1:
+                createNewEvent();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                cout << "este es el 4" << endl;
+                break;
+            default:
+                cout << "Este dato no esta permitido"<<endl;
+                break;
+            }
+        }
+    }
+    else {
+        cout << "Ha habido un error al cargar los datos, Finalizando programa."<<endl;
+    }
+}
+
+bool loadData(){
+    return true;
+}
+bool createNewEvent(){
+}
