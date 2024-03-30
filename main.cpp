b0eefd03f8f4c2d5c44d051417db856565ca0ef4
tomasplz <tomasplz@hotmail.com>
2024-03-30 19:54:39 -0300

    Se reordenan los directorios para tener mayor orden a la hora de trabajar con archivos y se agregan prototipos de funciones al main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
-#include "Evento.h"
+#include "../Classes/Evento.h"
 
 bool loadData();
 bool createNewEvent();
+bool registryAssistant();
+bool consultAssistants();
+bool generateInforms();
+bool saveData();
 
 using namespace std;
 
 int main(){
     int answer;
+    bool var = true;
     cout << "Cargando los datos . . .\n\n\n"<<endl;
-    if (loadData())
+    if (!loadData()){
+        cout << "Ha habido un error al cargar los datos, Finalizando programa."<<endl;
+    }
+    else
     {   
         cout << "Bienvenido al gestor de eventos\n"<<endl;
-        while (true)
+        while (var)
         {
             cout << "1) Crear nuevo evento \n2) Registro asistentes\n3) Consulta asistentes\n4) Generacion de informes"<<endl;
             cin >> answer;
@@ -22,11 +30,19 @@ int main(){
                     createNewEvent();
                     break;
                 case 2:
+                    registryAssistant();
                     break;
                 case 3:
+                    consultAssistants();
                     break;
                 case 4:
-                    cout << "este es el 3" << endl;
+                    generateInforms();
+                    break;
+                case 5:
+                    if (saveData())
+                    {
+                        var = false;
+                    };
                     break;
                 default:
                     cout << "Este dato no esta permitido"<<endl;
@@ -34,14 +50,27 @@ int main(){
             }
         }
     }
-    else {
-        cout << "Ha habido un error al cargar los datos, Finalizando programa."<<endl;
-    }
 }
 
 bool loadData(){
     return true;
 }
 bool createNewEvent(){
+    return true;
+}
+
+bool registryAssistant(){
+    return true;
+}
+
+bool consultAssistants(){
+    return true;
+}
+
+bool generateInforms(){
+    return true;
+}
+
+bool saveData(){
     return true;
 }
